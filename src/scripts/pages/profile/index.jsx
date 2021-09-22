02edf851254923e0947b6f7dea00f8035d2fafcb
wmdanor_PC <wmdanor@gmail.com>
2021-09-23 00:36:17 +0300

    Profile page with mock user getter and example profile image

--- src/scripts/pages/profile/index.jsx
+++ src/scripts/pages/profile/index.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import loggedUserProp from '../../../prop-types/loggedUser';
+import omegalulImage from '../../../images/omegalul.png';
 
 function mapStateToProps(state) {
   return {
@@ -8,26 +10,70 @@ function mapStateToProps(state) {
   };
 }
 
-const Index = ({ loggedUser }) => (
-  <div className="container py-3 px-5 shadow">
-    <div className="row">
-      <div className="col-12 col-sm-3 d-flex justify-content-center justify-content-sm-start">
-        <img src="/images/omegalul.png" alt="Omegalul meme" />
-      </div>
+const getUserById = async (id) => {
+  if (id === 0) {
+    throw new Error('Test error');
+  }
+
+  const promise = new Promise((resolve) => setTimeout(resolve, 500));
+  await promise;
+
+  return {
+    id,
+    email: 'example@email.com',
+    username: 'example',
+    age: 20,
+  };
+};
+
+const Index = ({ loggedUser }) => {
+  const { id } = useParams();
+
+  const [user, setUser] = useState(undefined);
+  const [loadingError, setLoadingError] = useState('');
+
+  useEffect(() => {
+    getUserById(Number(id))
+      .then((data) => setUser(data))
+      .catch((error) => setLoadingError(error.message));
+  }, []);
+
+  const isLoggedUserProfile = user && loggedUser && user.id === loggedUser.id;
+
+  return (
+    <div className="container py-3 px-5 shadow">
+      {!loadingError && user && (
+        <>
+          {isLoggedUserProfile && <h2>Your profile</h2>}
+          <div className="row">
+            <div className="col-12 col-sm-3 d-flex justify-content-center justify-content-sm-start">
+              <img src={omegalulImage} alt="Omegalul meme" />
+            </div>
+            <div className="col-12 col-sm-3 offset-sm-6 mt-3 mt-sm-0">
+              <div className="d-flex flex-column align-items-center align-items-sm-end">
+                <span>{user.username}</span>
+                <span>{user.email}</span>
+                <span>{user.age}</span>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+      {!loadingError && !user && (
+        <div className="my-4 d-flex">
+          <span className="text-center w-100">Loading...</span>
+        </div>
+      )}
+      {loadingError && (
+        <div className="my-4 d-flex">
+          <span className="text-center text-danger w-100">
+            Error occurred while loading profile: &quot;{loadingError}&quot;
+          </span>
+        </div>
+      )}
     </div>
-    <div className="col-12 col-sm-3 offset-sm-6">
-      <div className="d-flex flex-column align-items-center align-items-sm-end">
-        {loggedUser && (
-          <>
-            <span>{loggedUser.username}</span>
-            <span>{loggedUser.email}</span>
-            <span>{loggedUser.age}</span>
-          </>
-        )}
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 Index.propTypes = {
   loggedUser: loggedUserProp,
